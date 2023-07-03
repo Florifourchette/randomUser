de910514182ad9ba84d7c50000a7a0e85447e0ab
Florifourchette <florifourchette@gmail.com>
2023-07-03 10:57:39 +0200

    infiniteScroll done and API filtered

--- src/utils/Apicall.ts
+++ src/utils/Apicall.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function callRandomUSers(): Promise<Array<Object>> {
   return axios
     .get(
-      'https://randomuser.me/api/?results=25&inc=name,email,picture,location,id,phone'
+      'https://randomuser.me/api/?results=25&inc=name,email,picture,location,id,phone,gender'
     )
     .then((response) => {
       return response.data.results;
